0869fcce7ce6e97c958388415d1c6a8cac729cf0
Arnab Karmakar <arnab@binary.com>
2015-09-28 02:54:31 +0000

    Commiting v1.2.5 using TravisCI and GruntJS build process for prod

---
+++ v1.2.5/common.js
@@ -0,0 +1,1 @@
+function getParameterByName(a){a=a.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]");var b=new RegExp("[\\?&]"+a+"=([^&#]*)"),c=b.exec(location.search);return null===c?"":decodeURIComponent(c[1].replace(/\+/g," "))}
